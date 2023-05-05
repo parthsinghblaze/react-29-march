0f7491ad572ea45085d29621e1b9589c2206799f
parthsingh <parth@gmail.com>
2023-05-05 09:57:53 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { useState } from 'react';
+
 function App() {
+
+    // useState is a function which will return an array ;
+    // 1 -> initialValue
+    // 2 -> function this function will change the initialValue
+
+    const [userName, setUserName] = useState("Kalpesh"); 
+    const [number, setNumber] = useState(0);
+    const [isShowing, setIsShowing] = useState(true);
+
     return (
-        <div>
-            <h1>Hello i am APP</h1>
+        <div className='container'>
+            <h2>Boolean</h2>
+            <button onClick={() => setIsShowing(false)}>Hide</button>
+            <button>Show</button>
+            <button>Hide/show</button>
+            <br />
+            <br />
+            {
+                isShowing ? <img src='https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' width="200" /> :  null
+            }
+            <hr />
+            <h2>Working with number</h2>
+            <h1>{number}</h1>
+            <button onClick={() => setNumber(number + 1)}>+</button>
+            <button onClick={() => setNumber(number - 1)}>-</button>
+            <hr />
+            <h2>Working with String</h2>
+            <h1>My name is {userName}</h1>
+            <button onClick={() => setUserName("Alpesh")}>Change</button>
         </div>
     )
 }
---
+++ src/Product.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export function Product1() {
+  return (
+    <div>Product1</div>
+  )
+}
+
+export function Product2() {
+    return (
+      <div>Product2</div>
+    )
+  }
+  
+  
+
