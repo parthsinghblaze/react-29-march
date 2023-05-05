a02ffbf7e6450af739c4e4244f3ce29757ec4647
parthsingh <parth@gmail.com>
2023-05-05 09:34:45 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from './Header'
 import Slider from './Slider';
 import Footer from './Footer';
+import Product from './Product';
 
 function App() {
     return (
         <div>
             <Header />
             <Slider />
+            <Product />
             <Footer />
         </div>
     )
--- src/Slider.js
+++ src/Slider.js
@@ -1,5 +1,26 @@
 function Slider() {
-    return <div style={{ background: 'skyblue', height: '40vw' }}></div>
+    return <div id="carouselExample" className="carousel slide">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="d-block" style={{ height: "40vw", width: "100%", objectFit: "cover" }} alt="..." />
+    </div>
+    <div className="carousel-item">
+      <img src="https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="d-block" style={{ height: "40vw", width: "100%", objectFit: "cover" }} alt="..." />
+    </div>
+    <div className="carousel-item">
+      <img src="https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="d-block" style={{ height: "40vw", width: "100%", objectFit: "cover" }} alt="..." />
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true" />
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true" />
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+
 }
 
 export default Slider 
